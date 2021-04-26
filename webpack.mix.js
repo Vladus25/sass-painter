e8ca27a7e005e3379df8bfec89d91c3552dfed6a
Vladyslav Okopnyy <harmfulcati@gmail.com>
2021-04-26 19:03:12 +0300

    Jumbo completo

--- webpack.mix.js
+++ webpack.mix.js
@@ -1,2 +1,7 @@
 let mix = require("laravel-mix");
-mix.js("src/script.js", "dist").sass("src/style.scss", "").setPublicPath("dist").copy('src/index.html', 'dist');
+mix.options({
+    // Don't perform any css url rewriting by default
+    processCssUrls: false,
+})
+
+mix.js("src/script.js", "dist").sass("src/style.scss", "").setPublicPath("dist").options({processCssUrls: false,}).copy('src/index.html', 'dist');
